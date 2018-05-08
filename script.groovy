39bed12445ed7e428d8ffe487a224d640da2bd72
Ayoub Ait Lahcen <ayoub.aitlahcen95@gmail.com>
2018-05-08 15:44:20 +0100

    version de la multiplicite

--- script.groovy
+++ script.groovy
@@ -7,9 +7,9 @@ import jxl.*
 import jxl.write.*
 
 // ------------------------------- PARAMETRAGE -------------------------------
-def serviceID = 5
-def balisePrin = "customerEmailAddress"
-def serviceName = "getCustomerEmailAddressList"
+serviceID = 7
+def balisePrin = "customerBudgetDetail"
+serviceName = "getCustomerBudgetDetail"
 
 // ------------------------------- CONNEXION A LA BASE DE DONNEES AMPLITUDE -------------------------------
 com.eviware.soapui.support.GroovyUtils.registerJdbcDriver( "oracle.jdbc.driver.OracleDriver")
@@ -21,7 +21,7 @@ sql = Sql.newInstance(dbUrl, dbUser, dbPassword, dbDriver)
 // ------------------------------- RECUPERER LA REQUETE COMPLETE -------------------------------
 import groovyx.net.http.RESTClient
 
-def client = new RESTClient( 'http://localhost:8080' )
+client = new RESTClient( 'http://localhost:8080' )
 def resp = client.get( path : 'piste2/'+serviceID )
 
 Scanner scanner = new Scanner(resp.getData()).useDelimiter("\\A")
@@ -35,92 +35,117 @@ def testStep = testRunner.testCase.testSteps["pro"]
 
 testRunner.testCase.getTestStepByName("soapReq").run(testRunner,context)
 def groovyUtils = new com.eviware.soapui.support.GroovyUtils( context )
-def responseHolder = groovyUtils.getXmlHolder( testRunner.testCase.testSteps["soapReq"].testRequest.response.responseContent )
+responseHolder = groovyUtils.getXmlHolder( testRunner.testCase.testSteps["soapReq"].testRequest.response.responseContent )
 
 
-WritableWorkbook workbook1 = Workbook.createWorkbook(new File("d:\\Profiles\\aaitlahcen\\Desktop\\PERSO\\"+serviceName+".xls"))
-WritableSheet sheet1 = workbook1.createSheet("Rapport", 0)
+workbook1 = Workbook.createWorkbook(new File("d:\\Profiles\\aaitlahcen\\Desktop\\PERSO\\"+serviceName+".xls"))
+sheet1 = workbook1.createSheet("Rapport", 0)
 
-WritableFont cellFont = new WritableFont(WritableFont.TIMES, 12);
+cellFont = new WritableFont(WritableFont.TIMES, 12);
 cellFont.setColour(Colour.WHITE);
 
-WritableCellFormat cellFormatV = new WritableCellFormat(cellFont);
+cellFormatV = new WritableCellFormat(cellFont);
 cellFormatV.setBackground(Colour.GREEN);
 cellFormatV.setBorder(Border.ALL, BorderLineStyle.THIN);
 
-WritableCellFormat cellFormatNV = new WritableCellFormat(cellFont);
+cellFormatNV = new WritableCellFormat(cellFont);
 cellFormatNV.setBackground(Colour.RED);
 cellFormatNV.setBorder(Border.ALL, BorderLineStyle.THIN);
 sheet1.addCell(new Label(0, 0, "Le statut de la réponse"));
 sheet1.addCell(new Label(1, 0, responseHolder.getNodeValue("//fjs1:statusCode")));
 int x = 1
-int j = 1;
-sql.eachRow(req){row ->
-	def champsClient = client.get( path : 'xpath/'+serviceID )
-	for(String s : champsClient.getData()){
-		String[] split = s.split(":")[1].split("_")
-		String memeTable = s.split(":")[2]
-		String calculated = s.split(":")[3]
-		def xpath = "//fjs1:"+balisePrin+"[$x]";
-		for(int i = split.length - 1; i >= 0;i--){
-			xpath += "//fjs1:"+split[i]
-		}
-		String name = s.split(":")[1]
-		
-		String p = "champ/"+serviceID+"/"+name
-		def cClient = client.get( path : p)
-		def resultFromRest = new Scanner(cClient.getData()).useDelimiter("\\A").next()
-		def expected
-		def observed = responseHolder.getNodeValue(xpath).toString().trim()
-		if(memeTable.equals("true") && calculated.equals("false")){
-			expected = row."$resultFromRest".toString().trim()
-		}
-		else{
-			String[] splitReq = resultFromRest.split("\\*")
-			String param = splitReq[0].split(" ")[1]
-			String tagValue = testRunner.testCase.testSteps["proResponse"].getPropertyValue(splitReq[1])
-			//log.info splitReq[0] + " : "+tagValue
-			completeReq = splitReq[0].replaceAll("\\?",tagValue)
-			log.info name+" : "+completeReq
-			def myReq = sql.firstRow(completeReq)
-			if(calculated.equals("false"))
-				expected = myReq."$param".toString().trim()
-			else{
-				expected = myReq."$name".toString().trim()
-			}
-		}
-		//log.info expected+" : "+observed
-		sheet1.addCell(new Label(0, j, name));
-		if(name.startsWith("date")){
-			if(expected != "null")
-				expected = expected.substring(0,10);
-			if(observed != "null")
-				observed = observed.substring(0,10);
-		}
-		if(name.startsWith("amount") || name.contains("Rate") || name.contains("Capital")){
-			expected = Double.parseDouble(expected+"");
-			observed = Double.parseDouble(observed+"");
-			expected = String.format("%f",expected);
-			observed = String.format("%f",observed);
-		}
-		if(expected == "null")
-			expected = ""
-		if(observed == "null")
-			observed = ""
-		sheet1.addCell(new Label(1, j, expected));
-		sheet1.addCell(new Label(2, j, observed));
-		if(expected == observed){
-			sheet1.addCell(new Label(3, j, "PASS",cellFormatV));
-		}
-		else{
-			sheet1.addCell(new Label(3, j, "FAIL",cellFormatNV));
-		}
+j = 1
 
-		j++;
-		testRunner.testCase.testSteps["proResponse"].setPropertyValue(name,expected)
-	}
-	x++;
-}
-
+solve(req,1,balisePrin)
 workbook1.write()
 workbook1.close()
+
+def solve(req,x,balisePrin){
+	sql.eachRow(req){row ->
+		def champsClient = client.get( path : 'xpath/'+serviceID+'/'+balisePrin)
+		for(String s : champsClient.getData()){
+			if(s.split(":").length <= 1){
+				req = s.split("\\|")[0]
+				String tagValue = testRunner.testCase.testSteps["proResponse"].getPropertyValue(s.split("\\|")[1])
+				req = req.replaceAll('\\?',tagValue)
+				solve(req,1,s.split("\\|")[2])
+			}
+			else{
+
+				split = s.split(":")[1].split("_")
+				memeTable = s.split(":")[2]
+				calculated = s.split(":")[3]
+				multiple = "false"
+				def xpath = "//fjs1:"+balisePrin+"[$x]";
+				for(int i = split.length - 1; i >= 0;i--){
+					xpath += "//fjs1:"+split[i]
+				}
+
+				name = s.split(":")[1]
+				String p = "champ/"+serviceID+"/"+name
+				def cClient = client.get( path : p)
+				def resultFromRest = new Scanner(cClient.getData()).useDelimiter("\\A").next()
+				expected = ""
+				observed = responseHolder.getNodeValue(xpath).toString().trim()
+				/////////////////
+				observed = responseHolder.getNodeValue(xpath).toString().trim()
+
+				if(memeTable.equals("true") && calculated.equals("false")){
+					expected = row."$resultFromRest".toString().trim()
+				}
+
+				else{
+
+					String[] splitReq = resultFromRest.split("\\|")
+					String param = splitReq[0].split(" ")[1]
+
+					String tagValue = testRunner.testCase.testSteps["proResponse"].getPropertyValue(splitReq[1])
+					log.info tagValue
+					completeReq = splitReq[0].replaceAll("\\?",tagValue)
+					def myReq = sql.firstRow(completeReq)
+					if(calculated.equals("false"))
+						expected = myReq."$param".toString().trim()
+					else{
+						expected = myReq."$name".toString().trim()
+					}
+
+				}
+				sheet1.addCell(new Label(0, j, name));
+				if(name.startsWith("date")){
+					if(expected != "null")
+						expected = expected.substring(0,10);
+					if(observed != "null")
+						observed = observed.substring(0,10);
+				}
+				if(name.startsWith("amount") || name.contains("Rate") || name.contains("Capital")){
+					if(expected != "null"){
+						expected = Double.parseDouble(expected+"");
+						expected = String.format("%f",expected);
+					}
+					if(observed != "null"){
+						observed = Double.parseDouble(observed+"");
+						observed = String.format("%f",observed);
+					}
+				}
+				if(expected == "null")
+					expected = ""
+				if(observed == "null")
+					observed = ""
+				sheet1.addCell(new Label(1, j, expected));
+				sheet1.addCell(new Label(2, j, observed));
+				if(expected == observed){
+					sheet1.addCell(new Label(3, j, "PASS",cellFormatV));
+				}
+				else{
+					sheet1.addCell(new Label(3, j, "FAIL",cellFormatNV));
+				}
+
+				j++;
+				testRunner.testCase.testSteps["proResponse"].setPropertyValue(name,expected)
+			}
+			log.info name
+		}
+		x++;
+	}
+}
+
